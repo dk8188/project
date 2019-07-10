af24c80d28e172bc0d5d43abcc032429590b9849
kd <example@gmail.com>
2019-07-10 23:36:40 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+var leftBoarderWidth =1;
+let second =2;
+const pi= 3.14;
+
+console.log(second);
+
+// 6 types of data
+console.log(4/0);
+var number = 5;
+var string ="hello";
+null;
+var obj={}; // всегда фигурные скобки
+let person = {
+    name:"John",
+    age: 25,
+    isMariied:false
+};
+console.log(person["name"]);
+//let arr = ['plum.png','orange.png','apple.png'];
+//console.log(arr[2])
+//alert("hi!");
+//let answer = confirm("are you 18 yr?");
+//let answer = +prompt("are you 18 yr?","yes");
+//console.log(typeof(answer));
+//console.log("arr" +" - obj");
+//console.log(1+ +" - obj");
+let incr=10,
+    decr=10;
+
+console.log(++incr);
+console.log(--decr);
+console.log(5%2);
+console.log("2" == 2);
+
+let isChecked = true,
+    isClosed = false; 
+    
+console.log(isChecked && isClosed);
+console.log("love");
